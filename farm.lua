6409b82bb78773c60c78e7faee40728e51a120b5
venozeiei <armcraftwar@gmail.com>
2026-07-07 10:04:26 +0700

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -34,7 +34,8 @@ end
 
 _G.AutoFarm = Config.AutoFarm
 _G.TargetSlot = Config.TargetSlot
-_G.CurrentAction = "Initializing..." 
+_G.CurrentAction = "Initializing..."
+_G.SessionStartTime = _G.SessionStartTime or os.time() 
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -698,7 +699,7 @@ task.spawn(function()
                 "📍 <b>Status:</b> %s\n" ..
                 "🗺️ <b>Map:</b> %s\n" ..
                 "🔄 <b>Action:</b> <font color='#00ffff'>%s</font>",
-                level, maxLevelReq, formatNumber(displayXP), formatNumber(displayMaxXP), prestige, formatNumber(gold), formatNumber(gems), formatTime(goldBoostTime), formatTime(xpBoostTime), perkColor, totalPerks, ((totalPerks and totalPerks <= 45) and 50) or 100, debugInvStr, statusStr, displayMapString, _G.CurrentAction or "Idle"
+                level, maxLevelReq, formatNumber(displayXP), formatNumber(displayMaxXP), prestige, formatNumber(gold), formatNumber(gems), formatTime(goldBoostTime), formatTime(xpBoostTime), perkColor, totalPerks, ((level <= 45) and 50) or 100, debugInvStr, statusStr, displayMapString, _G.CurrentAction or "Idle"
             )
         end
     end)
@@ -844,10 +845,10 @@ if placeId == 14916516914 then
                 local currentPrestige = _G.LastPrestige or plr:GetAttribute("Prestige") or 0
                 local currentLevel = _G.LastLevel or plr:GetAttribute("Level") or 0
                 
-                -- ขาย 50 ชิ้นสำหรับ 0-45 ชิ้น, 100 ชิ้นสำหรับมากกว่า 45 ชิ้น
+                -- ขาย 50 ชิ้นสำหรับ Level 0-45, 100 ชิ้นสำหรับ Level มากกว่า 45
                 if currentPrestige == 0 or (currentPrestige == 1 and currentLevel < 20) then
                     requiredPerksToSell = 50
-                elseif (_G.PerksUUIDs and #_G.PerksUUIDs <= 45) then
+                elseif currentLevel <= 45 then
                     requiredPerksToSell = 50
                 else
                     requiredPerksToSell = 100
